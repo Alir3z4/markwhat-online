7970ba73b14fa183bec5e335d2c3af42a49d24b9
Alireza Savand <alireza.savand@gmail.com>
2014-05-05 00:44:59 +0400

    Simple and stupid form processing

--- whatwhat/views.py
+++ whatwhat/views.py
@@ -1,5 +1,27 @@
 from django.views.generic import TemplateView
+from django_markwhat.templatetags.markup import markdown
+from whatwhat.forms import WhatForm
 
 
 class IndexView(TemplateView):
     template_name = "whatwhat/index.html"
+
+    def get_context_data(self, **kwargs):
+        ctx = super(IndexView, self).get_context_data(**kwargs)
+        form = WhatForm()
+
+        ctx['form'] = form
+
+        if self.request.method == 'POST':
+            form = WhatForm(self.request.POST)
+
+            if form.is_valid():
+                ctx['markuped_text'] = markdown(form.cleaned_data['text'])
+
+            ctx['form'] = form
+
+        return ctx
+
+    def post(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
+
