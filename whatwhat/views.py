f2a91dc2d432e214dfd825628a395b5a00727a4a
Alireza Savand <alireza.savand@gmail.com>
2014-05-10 23:32:09 +0400

    No form processing on IndexView

--- whatwhat/views.py
+++ whatwhat/views.py
@@ -4,30 +4,10 @@ from django.http import HttpResponse, Http404, HttpResponseNotFound
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView, View
 from django_markwhat.templatetags.markup import markdown, textile, restructuredtext
-from whatwhat.forms import WhatForm
 
 
 class IndexView(TemplateView):
     template_name = "whatwhat/index.html"
-
-    def get_context_data(self, **kwargs):
-        ctx = super(IndexView, self).get_context_data(**kwargs)
-        form = WhatForm()
-
-        ctx['form'] = form
-
-        if self.request.method == 'POST':
-            form = WhatForm(self.request.POST)
-
-            if form.is_valid():
-                ctx['markuped_text'] = markdown(form.cleaned_data['text'])
-
-            ctx['form'] = form
-
-        return ctx
-
-    def post(self, request, *args, **kwargs):
-        return self.get(request, *args, **kwargs)
 
 
 class MarkItWhatView(View):
