ed44687edf9cdedec5393af2cb8666d585df20da
Alireza Savand <alireza.savand@gmail.com>
2014-05-10 21:36:42 +0400

    Add MarkItWhatView
    the main api ;)

--- whatwhat/views.py
+++ whatwhat/views.py
@@ -1,5 +1,9 @@
-from django.views.generic import TemplateView
-from django_markwhat.templatetags.markup import markdown
+import json
+import logging
+from django.http import HttpResponse, Http404, HttpResponseNotFound
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import TemplateView, View
+from django_markwhat.templatetags.markup import markdown, textile, restructuredtext
 from whatwhat.forms import WhatForm
 
 
@@ -25,3 +29,63 @@ class IndexView(TemplateView):
     def post(self, request, *args, **kwargs):
         return self.get(request, *args, **kwargs)
 
+
+class MarkItWhatView(View):
+    parsers = {
+        'textile': textile,
+        'markdown': markdown,
+        'restructuredtext': restructuredtext
+    }
+    @csrf_exempt
+    def dispatch(self, *args, **kwargs):
+        return super(MarkItWhatView, self).dispatch(*args, **kwargs)
+
+    def validate_request_data(self, request):
+        data = None
+        # import ipdb;ipdb.set_trace()
+        try:
+            data = json.loads(request.body)
+        except Exception as exc:
+            logging.error("Invalid request: {0}".format(exc))
+            raise Http404
+
+        return data
+
+    def validate_parser(self, parser_name):
+        """
+        :type parser_name: basestring
+
+        :rtype: bool
+        """
+        if parser_name not in self.parsers:
+            return False
+
+        return True
+
+
+    def get_parser_class(self, parser_name):
+        """
+        :type parser_name: basestring
+        """
+        return self.parsers[parser_name]
+
+    def post(self, request, *args, **kwargs):
+        data = self.validate_request_data(request)
+        parser_name = data['parser']
+        if not self.validate_parser(parser_name):
+            return HttpResponseNotFound(
+                json.dumps({
+                    'text': 'Invalid parser: {0}'.format(parser_name)
+                })
+            )
+
+        parser = self.get_parser_class(parser_name)
+        payload = {
+            'text': parser(data['what'])
+        }
+
+        return HttpResponse(
+            status=201,
+            content_type='application/json',
+            content=json.dumps(payload)
+        )
